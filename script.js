81073dc2f51730d8487f351d8b5b2f5974a86277
Godwin <agblezeg@gmail.com>
2022-08-27 10:47:36 +0200

    Add winner message

--- script.js
+++ script.js
@@ -4,24 +4,34 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
+let playerCounter = 0;
+let ComputerCounter = 0;
+
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "paper") {
+    ComputerCounter++;
     return "You Lose! Paper beats Rock";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerCounter++;
     return "You Win! Rock beats Scissors";
   } else if (playerSelection === "rock" && computerSelection === "rock") {
     return "It's a draw";
 
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    ComputerCounter++;
     return "You Lose! Scissors beats Paper";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerCounter++;
     return "You Win! Paper beats Rock";
   } else if (playerSelection === "paper" && computerSelection === "paper") {
     return "It's a draw";
   
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    ComputerCounter++;
     return "You Lose! Rock beats Scissors";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerCounter++;
     return "You Win! Scissors beats Paper";
   } else if (playerSelection === "scissors" && computerSelection === "scissors") {
     return "It's a draw";
@@ -38,7 +48,14 @@ function game() {
 
     alert(playRound(playerSelection.toLowerCase(), computerSelection));
     console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+  }
 
+  if (playerCounter > ComputerCounter) {
+    alert("You are the winner of the game!");
+  } else if (playerCounter < ComputerCounter) {
+    alert("The Computer is the winner of the game!");
+  } else {
+    alert("No winner. It's a draw!");
   }
 }
 
