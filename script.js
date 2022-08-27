f0c68b8216409cf2a5056dc1afe41c647f24dfd4
Godwin <agblezeg@gmail.com>
2022-08-27 10:11:45 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 function getComputerChoice() {
-  const choices = ["Rock", "Paper", "Scissors"];
+  const choices = ["rock", "paper", "scissors"];
 
-  console.log(choices[Math.floor(Math.random() * choices.length)]);
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
+/*
+
+Write a function that plays a single round of rock paper scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! paper beats rock"
+Make your function’s playerSelection parameter case-insensitive (so users can input rock, rock, rock or any other variation).
+Important note: you want to return the results of this function call, not console.log() them. You’re going to use what you return later on, so let’s test this function by using console.log to see the results:
+
+*/
+
+function playRound(playerSelection, computerSelection) {
+  // my code here
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection === "rock" && computerSelection === "rock") {
+    return "It's a draw";
+
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "paper") {
+    return "It's a draw";
+  
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissors beats Paper";
+  } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+    return "It's a draw";
+  }
+
+}
+
+const playerSelection = "scIssORS";
+
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection.toLowerCase(), computerSelection));
