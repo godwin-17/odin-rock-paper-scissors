417e04768783dcf32e24ff0acaabfcf312d97a01
Godwin <agblezeg@gmail.com>
2022-08-27 10:30:45 +0200

    Add game function

--- script.js
+++ script.js
@@ -4,16 +4,7 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-/*
-
-Write a function that plays a single round of rock paper scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! paper beats rock"
-Make your function’s playerSelection parameter case-insensitive (so users can input rock, rock, rock or any other variation).
-Important note: you want to return the results of this function call, not console.log() them. You’re going to use what you return later on, so let’s test this function by using console.log to see the results:
-
-*/
-
 function playRound(playerSelection, computerSelection) {
-  // my code here
   if (playerSelection === "rock" && computerSelection === "paper") {
     return "You Lose! Paper beats Rock";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
@@ -38,8 +29,17 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-const playerSelection = "scIssORS";
+function game() {
+  
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Rock, Paper or Scissors");
 
-const computerSelection = getComputerChoice();
+    let computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+    alert(playRound(playerSelection.toLowerCase(), computerSelection));
+    console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+
+  }
+}
+
+game();
